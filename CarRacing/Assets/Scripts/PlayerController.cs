1381c319ffe25c186280d3da246fca376129eb14
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-12-22 18:46:02 +0330

    add car and car controller

--- CarRacing/Assets/Scripts/PlayerController.cs
+++ CarRacing/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 
@@ -7,7 +8,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 1.0f;
-    private float direction;
+    private float horizantalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +19,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Input.GetAxisRaw("Horizontal");
-
-
+        MoveGameObject();
     }
-    private void FixedUpdate()
+    private void MoveGameObject()
     {
-        rb.velocity = Vector2.zero;
-        rb.velocity = new Vector2(direction * Time.fixedTime * moveSpeed, 0);
+        horizantalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.transform.Translate(Vector2.right * horizantalInput * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.transform.Translate(Vector2.up * verticalInput * Time.deltaTime * moveSpeed);
+        }
+        //rb.velocity = Vector2.zero;
+        //rb.velocity = new Vector2(direction * Time.fixedTime * moveSpeed, 0);
+
     }
 }
