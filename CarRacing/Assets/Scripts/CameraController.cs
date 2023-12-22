4b6d4d7ba6256bc0b4a02811b68dd4f39a9344bd
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-12-22 19:46:14 +0330

    add car light and its controller

--- CarRacing/Assets/Scripts/CameraController.cs
+++ CarRacing/Assets/Scripts/CameraController.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
-
-public class PlayerController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 1.0f;
     private float horizantalInput;
     private float verticalInput;
-    private Light SpotLightR;
-    private Light SpotLightL;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        SpotLightR = GetComponent<Light>();
-        SpotLightL = GetComponent<Light>();
     }
 
     // Update is called once per frame
@@ -37,25 +31,6 @@ public class PlayerController : MonoBehaviour
         {
             rb.transform.Translate(Vector2.up * verticalInput * Time.deltaTime * moveSpeed);
         }
-        //rb.velocity = Vector2.zero;
-        //rb.velocity = new Vector2(direction * Time.fixedTime * moveSpeed, 0);
 
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Tunnel")
-        {
-            SpotLightR.enabled = !SpotLightR.enabled;
-            SpotLightL.enabled = !SpotLightL.enabled;
-        }
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Tunnel")
-        {
-            SpotLightR.enabled = !SpotLightR.enabled;
-            SpotLightL.enabled = !SpotLightL.enabled;
-        }
-    }
-
 }
